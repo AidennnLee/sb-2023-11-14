4f3b5be3715f61b65ac38c22f7aafd33d29b2887
Aidennn-Lee <yw010804@gmail.com>
2023-11-14 10:44:10 +0900

    C:/Program Files/Git/calc, /calc2

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -17,4 +17,16 @@ public class HomeController {
     String showAbout(){
         return "개발자 커뮤니티";
     }
+
+    @GetMapping("/calc")
+    @ResponseBody
+    String showCalc(int a, int b){
+        return "result : " + (a + b);
+    }
+
+    @GetMapping("/calc2")
+    @ResponseBody
+    String showCalc(Integer a, Integer b){
+        return "a : " + a + " b : " + b;
+    }
 }
