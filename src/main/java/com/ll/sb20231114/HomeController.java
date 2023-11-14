cbc56c3b1d1f29b173790676f0b7fff15631b425
Aidennn-Lee <yw010804@gmail.com>
2023-11-14 10:57:02 +0900

    calc3, calc4, calc5

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -26,7 +27,34 @@ public class HomeController {
 
     @GetMapping("/calc2")
     @ResponseBody
-    String showCalc(Integer a, Integer b){
+    String showCalc2(Integer a, Integer b){
         return "a : " + a + " b : " + b;
+    }
+
+    @GetMapping("/calc3")
+    @ResponseBody
+    String showCalc3(
+            @RequestParam(defaultValue = "0") int a,
+            @RequestParam(defaultValue = "0") int b
+    ){
+        return "result : " + (a + b);
+    }
+
+    @GetMapping("/calc4")
+    @ResponseBody
+    String showCalc4(
+            @RequestParam(defaultValue = "0") double a,
+            @RequestParam(defaultValue = "0") double b
+    ){
+        return "result : " + (a + b);
+    }
+
+    @GetMapping("/calc5")
+    @ResponseBody
+    String showCalc5(
+            @RequestParam(defaultValue = "0") String a,
+            @RequestParam(defaultValue = "0") String b
+    ){
+        return a + b;
     }
 }
