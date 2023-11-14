a4f17ba355b35a4ba8ddf5ece346da9fab3e3d6e
Aidennn-Lee <yw010804@gmail.com>
2023-11-14 14:11:17 +0900

    calc8 ~ calc12

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -1,9 +1,15 @@
 package com.ll.sb20231114;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -75,4 +81,65 @@ public class HomeController {
     ) {
         return a > b;
     }
+
+    @GetMapping("/calc8")
+    @ResponseBody
+    Person showCalc8(
+            String name, int age
+    ) {
+        return new Person(name, age);
+    }
+
+    @GetMapping("/calc9")
+    @ResponseBody
+    Person2 showCalc9(
+            String name, int age
+    ) {
+        return new Person2(name, age);
+    }
+
+    @GetMapping("/calc10")
+    @ResponseBody
+    Map<String, Object> showCalc10(
+            String name, int age
+    ) {
+        Map<String, Object> personMap = Map.of(
+                "name", name,
+                "age", age
+        );
+        return personMap;
+    }
+
+    @GetMapping("/calc11")
+    @ResponseBody
+    List<Integer> showCalc11(){
+        List<Integer> nums = new ArrayList<>(){{
+            add(10);
+            add(-500);
+            add(10010);
+        }};
+        return nums;
+    }
+
+    //calc11과 calc12의 출력 형태는 동일
+    @GetMapping("/calc12")
+    @ResponseBody
+    int[] showCalc12(){
+        int[] nums = new int[]{10, -510, 10010};
+        return nums;
+    }
+}
+
+@AllArgsConstructor
+class Person{
+    public String name;
+    public int age;
+}
+
+@AllArgsConstructor
+class Person2{
+    @Getter
+    private String name;
+    @Getter
+    private int age;
 }
